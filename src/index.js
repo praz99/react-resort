5a88175f4c52cd5e8072cda1aa034d98e76d8237
Praz <t.prazwal@gmail.com>
2021-03-08 01:19:14 +0100

    Add Context.js create RoomContext, RoomProvider and RoomConsumer Add RoomProvider in in index.js

--- src/index.js
+++ src/index.js
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
+import { RoomProvider } from './Context';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <RoomProvider>
+      <Router>
+        <App />
+      </Router>
+    </RoomProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
