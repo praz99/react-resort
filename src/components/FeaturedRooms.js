b7a8b0ab2736bc93c365d9967187e78f23f8a2a0
Praz <t.prazwal@gmail.com>
2021-03-09 01:11:13 +0100

    display Room and Loading components in FeaturedRooms

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
 import { RoomContext } from '../Context';
 import Loading from './Loading';
+import Title from './Title';
 import Room from './Room';
 
 export default class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render () {
-    const {featuredRooms: rooms} = this.context
+    let {loading, featuredRooms: rooms} = this.context
+    if (rooms) {
+      rooms = rooms.map(room => {
+        return (<Room key={room.id} room={room} />)
+      });
+    }
+
     console.log(rooms);
     return (
-      <div>
-        <Room />
-        <Loading />
-      </div>
+      <section className="featured-rooms">
+        <Title title="featured rooms" />
+        <div className="featured-rooms-center">
+          {loading ? <Loading /> : rooms}
+        </div>
+      </section>
     );
   };
 };
