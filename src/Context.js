c05582ec01f8a84537ea8c159f0b279ebebe9fb5
Praz <t.prazwal@gmail.com>
2021-03-12 20:28:33 +0100

    add handleChange and filterRooms methods to Context

--- src/Context.js
+++ src/Context.js
@@ -9,16 +9,30 @@ class RoomProvider extends Component {
     sortedRooms: [],
     featuredRoom: [],
     loading: true,
+    type: 'All',
+    capacity: 1,
+    price: 0,
+    minPrice: 0,
+    maxPrice: 0,
+    minSize: 0,
+    maxSize: 0,
+    breakfast: false,
+    pets: false,
   };
 
   componentDidMount() {
     let rooms = this.formatData(items)
     let featuredRooms = rooms.filter(room => room.featured === true);
+    let maxPrice = Math.max(...rooms.map(item => item.price));
+    let maxSize = Math.max(...rooms.map(item => item.size));
     this.setState({
       rooms,
       featuredRooms,
       sortedRooms: rooms,
-      loading: false
+      loading: false,
+      price: maxPrice,
+      maxPrice,
+      maxSize,
     });
   };
 
@@ -38,11 +52,22 @@ class RoomProvider extends Component {
     return room;
   };
 
+  handleChange = event => {
+    const type = event.target.type
+    const name = event.target.name
+    const value = event.target.value
+  };
+
+  filterRooms = () => {
+    console.log('hello');
+  }
+
   render() {
     return (
       <RoomContext.Provider value={{ 
         ...this.state,
-        getRoom: this.getRoom
+        getRoom: this.getRoom,
+        handleChange: this.handleChange,
       }}>
         {this.props.children}
       </RoomContext.Provider>
