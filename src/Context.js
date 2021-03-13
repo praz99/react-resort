7f900ee7d06b912a679e1bd5897977ed11829798
Praz <t.prazwal@gmail.com>
2021-03-13 01:52:53 +0100

    add room filter for filtering by room type

--- src/Context.js
+++ src/Context.js
@@ -53,13 +53,23 @@ class RoomProvider extends Component {
   };
 
   handleChange = event => {
-    const type = event.target.type
-    const name = event.target.name
-    const value = event.target.value
+    const target = event.target;
+    const value = event.type === 'checkbox' ? target.checked : target.value;
+    const name = event.target.name;
+    this.setState({
+      [name]: value
+    }, this.filterRooms)
   };
 
   filterRooms = () => {
-    console.log('hello');
+    let { rooms, type, capacity, price, minSize, maxSize, breakfast, pets} = this.state;
+    let tempRooms = [...rooms];
+    if(type !== 'all') {
+      tempRooms = tempRooms.filter(room => room.type === type);
+    }
+    this.setState({
+      sortedRooms: tempRooms,
+    })
   }
 
   render() {
